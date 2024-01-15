73afe4d5e83d7af6cd95bc4e05e361b0e1f20d6f
casavaca <96765450+casavaca@users.noreply.github.com>
2024-01-14 19:08:28 -0800

    Add comments

--- src/game_event.hpp
+++ src/game_event.hpp
@@ -1,5 +1,34 @@
 #pragma once
 
+// Suppose you have a 2D FPS game,
+//
+// GameEvent actually consists of 2 components,
+// Those that should be handled by a game.
+//   e.g., movement, shoot, etc.
+// Those that should be handled by gui library
+//   e.g., button buy_a_gun clicked.
+//
+// Suppose that game core engine doesn't care how it is displayed.
+// So, buy_a_gun event needs to be converted into
+//     game_add_gun         XXX
+//     game_switch_to_gun   XXX
+//     game_decrease_money $YYY
+//
+// We could just do this in the gui:
+// ```
+//   if (buy_a_gun clicked) {
+//     game_add_gun         XXX
+//     game_switch_to_gun   XXX
+//     game_decrease_money $YYY
+//   }
+// ```
+// But how do you test it?
+// Here, we make the gui code return all gui events:
+// `auto gui_events = Gui::processInput()`. // gui_events contains buy_a_gun
+// `Gui::processEvents(gui_events)`.        // then process them.
+//
+// In this case, we can mock Gui::processInput easily.
+
 enum class GameEvent {
     EVENT_NULL,
 
